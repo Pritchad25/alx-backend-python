207230ded73347a1aab9ce56a70cc6e4a16be971
Pritchad25 <pricts29@gmail.com>
2024-01-11 23:37:28 +0000

    a type-annotated function which takes a float as argument and returns the floor of the float.

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Function returns the floor of a float """
+
+
+def floor(n: float) -> int:
+    """ This function takes float, returns a rounded int """
+    return (int(n))
