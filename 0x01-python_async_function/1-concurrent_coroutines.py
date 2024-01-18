fcf970dd98ee1a70a4cbb1f1facc6cb7041f0012
Pritchad25 <pricts29@gmail.com>
2024-01-18 23:11:29 +0000

    including variable that will help spawn wait-random n time.

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,16 +4,14 @@ random delay """
 
 
 import asyncio
-import random
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
-    """ Async that waits for ran delay until max_delay,
-    returns list of actual delays """.
-
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    execute multiple coroutines at the same time with async
+    """
     values = []
 
     for _ in range(n):
