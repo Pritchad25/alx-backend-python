d1c892c8eda40faeee0932e446d5f9ffdfa27a2f
Pritchad25 <pricts29@gmail.com>
2024-02-04 20:14:06 +0000

    Adding unitests for GithubOrgClient.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,74 +1,39 @@
 #!/usr/bin/env python3
-""" The Unittest module """
+""" A script for unit tests of uclient.GithubOrgClient class.
+"""
 
-from unittest import TestCase, mock
-from unittest.mock import patch, Mock
+import unittest
 from parameterized import parameterized
-
+from unittest import mock
+from unittest.mock import PropertyMock, patch
+import requests
 import client
 from client import GithubOrgClient
 
 
-class TestGithubOrgClient(TestCase):
-    """A Class for testing GithubOrgClient """
-
+class TestGithubOrgClient(unittest.TestCase):
+    """A class that implements the test_org method.
+    """
     @parameterized.expand([
-        ("google"),
-        ("abc"),
+        ('google'),
+        ('abc')
     ])
     @patch('client.get_json')
-    def test_org(self, org_name, mock_json):
-        """ The Test method returns correct output """
-        gc = GithubOrgClient(org_name)
-        gc.org()
-        mock_json.assert_called_once_with(
-            f"https://api.github.com/orgs/{org_name}"
-            )
-        self.assertEqual(wrong_output, e.exception)
+    def test_org(self, input, mock):
+        """A test method that GithubOrgClient.org returns the correct value."""
+        test_class = GithubOrgClient(input)
+        test_class.org()
+        mock.called_with_once(test_class.ORG_URL)
+
+    def test_public_repos_url(self):
+        """A unit-test method for GithubOrgClient._public_repos_url"""
+        with patch.object(GithubOrgClient, '_public_repos_url',
+                          new_callable=PropertyMock) as mock_property:
+            mock_property.return_value = 'mock_value'
+            inst = GithubOrgClient('org_name')
+
+            self.assertEqual(inst._public_repos_url, 'mock_value')
 
 
-class TestGetJson(TestCase):
-    """ A Class for testing get_json function """
-    # order of args: test_url, test_payload
-    @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False})
-    ])
-    def test_get_json(self, test_url, test_payload):
-        """The Test method returns correct output """
-        # create Mock object with json method that returns test_payload
-        mock_response = Mock()
-        mock_response.json.return_value = test_payload
-        # function calls requests.get, need patch to get mock return value
-        with patch('requests.get', return_value=mock_response):
-            real_response = get_json(test_url)
-            self.assertEqual(real_response, test_payload)
-            # check that mocked method called once per input
-            mock_response.json.assert_called_once()
-
-
-class TestMemoize(TestCase):
-    """A Class for testing memoization """
-
-    def test_memoize(self):
-        """A Tests memoize function """
-
-        class TestClass:
-            """A Test class """
-
-            def a_method(self):
-                """A Method to always return 42 """
-                return 42
-
-            @memoize
-            def a_property(self):
-                """ The property returns memoized property """
-                return self.a_method()
-
-        with patch.object(TestClass, 'a_method', return_value=42) as patched:
-            test_class = TestClass()
-            real_return = test_class.a_property
-            real_return = test_class.a_property
-
-            self.assertEqual(real_return, 42)
-            patched.assert_called_once()
+if __name__ == '__main__':
+    unittest.main()
