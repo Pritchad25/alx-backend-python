4440ed97fe18cf9997c4d09270ff87a76cabfb7c
Pritchad25 <pricts29@gmail.com>
2025-05-25 07:35:59 +0000

    add datetime import line.

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,14 +1,15 @@
 import sqlite3
 import functools
+from datetime import datetime  
 
 #### decorator to lof SQL queries
 def log_queries(func):
-        """Decorator to log SQL queries before execution."""
+        """Decorator to log SQL queries before execution with timestamp."""
             @functools.wraps(func)
                 def wrapper(*args, **kwargs):
-                            # Assuming the first argument is the SQL query
-                                    query = args[0] if args else kwargs.get("query", "")
-                                            print(f"Executing SQL Query: {query}")
+                            query = args[0] if args else kwargs.get("query", "")
+                                    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                                            print(f"[{timestamp}] Executing SQL Query: {query}")
                                                     return func(*args, **kwargs)
                                                     
                                                     return wrapper
