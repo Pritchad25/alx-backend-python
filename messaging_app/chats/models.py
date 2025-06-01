6302aa01fdf4fbc50b37493e795617ce957230c7
Pritchad25 <pricts29@gmail.com>
2025-06-01 12:30:17 +0000

    Model designs for user, messages and conversations.

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    """Custom User model extending Django's default User model."""
+    bio = models.TextField(blank=True, null=True)
+
+class Conversation(models.Model):
+    """Model to track user conversations."""
+    participants = models.ManyToManyField(CustomUser,
+                                      related_name="conversations")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Message(models.Model):
+    """Model for messages sent in a conversation."""
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
