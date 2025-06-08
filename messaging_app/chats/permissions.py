383f6b905ea324f0c9412e4727b422b3b47ff3e7
Pritchad25 <pricts29@gmail.com>
2025-06-08 13:36:26 +0000

    Add permissions.

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,6 +1,13 @@
 from rest_framework import permissions
 
 class IsParticipantOfConversation(permissions.BasePermission):
+    """
+    Allows access only to authenticated users who are participants in the
+    conversation.
+    """
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+    
     def has_object_permission(self, request, view, obj):
+        ## Ensure the user is a participant in the conversation
         return obj.sender == request.user or obj.receiver == request.user
-
