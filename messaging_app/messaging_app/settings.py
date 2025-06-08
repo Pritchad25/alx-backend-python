86138d55c9afaee4048f06f5b449f6c786e76051
Pritchad25 <pricts29@gmail.com>
2025-06-08 13:26:20 +0000

    Adjust default authentication classes in settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS += ['chats']
 REST_FRAMEWORK = {
         'DEFAULT_AUTHENTICATION_CLASSES': (
             'rest_framework_simplejwt.authentication.JWTAuthentication',
+            'rest_framework.authentication.BasicAuthentication',
+            'rest_framework.authentication.SessionAuthentication',
         ),
 }
 
