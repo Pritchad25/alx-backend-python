479e0df7d8442e12f7ae316ff72091cccbbf8692
Pritchad25 <pricts29@gmail.com>
2025-06-08 12:39:45 +0000

    Implement Authentication.

---
+++ messaging_app/chats/permissions.py
@@ -0,0 +1,6 @@
+from rest_framework import permissions
+
+class IsParticipantOfConversation(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.sender == request.user or obj.receiver == request.user
+
